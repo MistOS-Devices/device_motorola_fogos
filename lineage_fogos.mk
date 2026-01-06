809c4e81e27ad9db9709dc9960d26c0abb7597ff
Ankit Kene <78631702+lazyDev-ak@users.noreply.github.com>
2026-01-07 03:05:06 +0530

    Update lineage_fogos.mk

--- lineage_fogos.mk
+++ lineage_fogos.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/motorola/fogos/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# MAINTAINER 
+MISTOS_MAINTAINER := JNWSG_(JINWOO)
+
 PRODUCT_NAME := lineage_fogos
 PRODUCT_DEVICE := fogos
 PRODUCT_MANUFACTURER := motorola
